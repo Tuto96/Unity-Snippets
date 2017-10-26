5821355391e3d9f3d43a06487eaddd1ff7c8a892
Carlos Augusto Restrepo <pg05carlos@vfs.com>
2017-10-26 10:35:27 -0700

    no message

---
+++ UI/Tooltip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GUIContent))]
+public class Tooltip : MonoBehaviour {
+
+	GUIContent gui;
+	public string tooltipText;
+
+	void OnGUI()
+	{
+		gui = GetComponent<GUIContent>();
+		gui.tooltip = tooltipText;
+	}
+}
---
+++ UI/UISliderShowNumber.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UISliderShowNumber : MonoBehaviour {
+    Text t;
+    Slider s;
+	// Use this for initialization
+	void Start () {
+        t = GetComponent<Text>();
+        s = GetComponentInParent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.text = s.value.ToString();
+	}
+}
